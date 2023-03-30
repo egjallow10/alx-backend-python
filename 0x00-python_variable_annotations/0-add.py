ffa4b45d791389d2b29110c8feed679de3cc9f51
egjallo <ejallow@insistglobal.com>
2023-03-30 23:25:44 +0000

    add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""funtion that add float"""
+"""Funtion that add float
+"""
 
 
 def add(a: float, b: float) -> float:
     """returns float"""
-    return a + b;
+    return (a + b)
