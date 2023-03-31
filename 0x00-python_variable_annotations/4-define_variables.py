89add8b9a0a5705b2bc76a138ccaf0e5b4d8ad52
egjallo <ejallow@insistglobal.com>
2023-03-31 00:45:25 +0000

    define

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Define and annotate the following variables 
+i_understand_annotations, a boolean with a value of True
+school, a string with a value of “Holberton”
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
