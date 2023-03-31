2a3977e0ffa72db7bf1e7f088a1d1278c8a4a5e5
egjallo <ejallow@insistglobal.com>
2023-03-31 00:07:54 +0000

    concat strings

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""Function that concat two strings 
+"""Function string str1
+and a string str2 as arg and returns a con string
 """
 
 
-def concat(str1 : str, str2: str)  -> str:
-    """Retruns a string concats"""
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings"""
     return str1 + str2
