4a750d6225745ffde39c07cd6ab16f2edecb5a57
egjallo <ejallow@insistglobal.com>
2023-03-31 00:47:51 +0000

    define

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""Define and annotate the following variables 
+"""Define and annotate the following variables with the specified values:
+a, an integer with a value of 1
+pi, a float with a value of 3.14
+i_understand_annotations, a boolean with a value of True
 school, a string with a value of “Holberton”
 """
 
