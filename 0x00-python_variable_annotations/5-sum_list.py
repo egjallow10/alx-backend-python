59e61615ecb6229b3aa99266db55600263a26509
egjallo <ejallow@insistglobal.com>
2023-03-31 00:59:49 +0000

    sum of list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+list of input_list of floats as argument
+returns  sum as a float
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Return the sum from the list
+    """
+    return float(sum(input_list))
+
+    
