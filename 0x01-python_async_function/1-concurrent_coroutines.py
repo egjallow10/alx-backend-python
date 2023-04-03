9edb8a79f891ac083e07eb4896e31f02e0084e3d
egjallow10 <egjallow10@gmail.com>
2023-04-03 23:37:10 +0000

    array of floats

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,16 +9,7 @@
 import asyncio
 from typing import List
 from random import uniform
-
-
-async def wait_random(max_delay: float = 10) -> float:
-    """
-    Asynchronous coroutine that waits  few seconds
-    and eventually returns .
-    """
-    delay = uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: float) -> List[float]:
