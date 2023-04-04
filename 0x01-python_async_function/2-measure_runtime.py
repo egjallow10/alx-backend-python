e8c31bd47721f40fd814736c8830cf8902f1611a
egjallow10 <egjallow10@gmail.com>
2023-04-04 00:07:45 +0000

    measure runtim

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Function should return a float.
+and iport from other files
+"""
+
+
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Returns time"""
+    init_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    total_time = end_time - init_time
+    return (total_time/n)
