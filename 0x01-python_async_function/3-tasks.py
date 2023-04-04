e45854bda5a5f3394072570fe9288ab18c61182d
egjallow10 <egjallow10@gmail.com>
2023-04-04 00:19:19 +0000

    measure runtim class

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,7 +8,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     A task that waits for a random delay
