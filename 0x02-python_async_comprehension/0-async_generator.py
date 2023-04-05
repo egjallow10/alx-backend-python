7826ca192a47ff21e894382af88aaf516398dcbd
egjallow10 <egjallow10@gmail.com>
2023-04-05 00:12:53 +0000

    async gennerator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""_summary_
+    Yields:
+        _type_: _description_
+        The coroutine will loop 10 times,
+        each time asynchronously wait 1 second, then yield a random number 
+    """
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """Retruns random numbers"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
