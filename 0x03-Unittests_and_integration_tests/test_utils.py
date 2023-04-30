c0e6b70f28c9c73ac26428ebd19a31c22e8c0e86
egjallow10 <egjallow10@gmail.com>
2023-04-30 07:56:04 +0000

    expection

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,12 +21,3 @@ class TestAccessNestedMap(unittest.TestCase):
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
 
-    @parameterized.expand([
-        ({}, ['a']),
-        ({'a': 1}, ['a', 'b'])
-        ])
-    
-    def test_access_nested_map_exception(self, nested_map, path, expected_result):
-        """Excetion"""
-        self.assertRaises(KeyError)
-
